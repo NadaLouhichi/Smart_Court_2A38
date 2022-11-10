e3e2a4f4632a080585741116963ef09422a9522a
chahed loumi <chahed.loumi@esprit.tn>
2022-11-10 14:06:56 +0100

    First Commit

--- Atelier_Connexion/connection.cpp
+++ Atelier_Connexion/connection.cpp
@@ -9,14 +9,11 @@ bool Connection::createconnect()
 {bool test=false;
 QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
 db.setDatabaseName("source_projet2A");
-db.setUserName("system");//inserer nom de l'utilisateur
-db.setPassword("24395408");//inserer mot de passe de cet utilisateur
+db.setUserName("system");// nom de l'utilisateur
+db.setPassword("24395408");//mot de passe de cet utilisateur
 
 if (db.open())
 test=true;
-
-
-
 
 
     return  test;
